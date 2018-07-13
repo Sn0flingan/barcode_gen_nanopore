315a24cf8ba1f78ff2ebc6fa870db40c4f6f6f23
Sn0flingan <alice.anlind@gmail.com>
2018-07-13 14:13:21 +0200

    Add distance option and code for barcodes

--- barcode_gen.py
+++ barcode_gen.py
@@ -6,14 +6,20 @@
 #
 
 import argparse
-import random
+from random import choice
+from Levenshtein import distance
 
 def main():
     args = get_arguments()
     barcodes =[]
     for i in range(args.numOfBc):
-        barcode = generate_barcode(args.length, [])
-        barcodes.append(''.join(barcode))
+        barcode = generate_barcode(args.length, "")
+        if len(barcodes) == 0:
+            barcodes.append(barcode)
+            continue
+        while min(distance(barcode,previous_bc) for previous_bc in barcodes)<=args.distance:
+            barcode = generate_barcode(args.length, "")
+        barcodes.append(barcode)
     print(barcodes)
     return
 
@@ -25,17 +31,19 @@ def get_arguments():
                         default=2, type=int)
     parser.add_argument("-o", "--output", help="name of output file",
                         default="barcodes.txt")
+    parser.add_argument("-d", "--distance", help="minimum Levenshtein distance between barcodes",
+                        default=2, type=int)
     args = parser.parse_args()
     return args
 
 def generate_barcode(bc_len, bc):
     if bc_len == 0:
         return bc
-    next_base = random.choice("atgc")
+    next_base = choice("atgc")
     if not len(bc) == 0:
         while next_base == bc[-1]:
-            next_base = random.choice("atgc")
-    bc.append(next_base)
+            next_base = choice("atgc")
+    bc += next_base
     return generate_barcode(bc_len-1, bc)
 
 main()
