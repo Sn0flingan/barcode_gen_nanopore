6ca5171445179fc9e873b13bfe1ee88fff365fec
Sn0flingan <alice.anlind@gmail.com>
2018-07-13 16:03:38 +0200

    Add verbosity option

--- barcode_gen.py
+++ barcode_gen.py
@@ -11,17 +11,18 @@ from Levenshtein import distance
 
 def main():
     args = get_arguments()
+    if args.verbosity >= 1:
+        print("\n--- Generating barcodes ---")
     barcodes =[]
     for i in range(args.numOfBc):
         barcode = generate_barcode(args.length, "")
-        if len(barcodes) == 0:
-            barcodes.append(barcode)
-            continue
-        while min(distance(barcode,previous_bc) for previous_bc in barcodes)<=args.distance:
-            barcode = generate_barcode(args.length, "")
+        if not len(barcodes) == 0:
+            while min(distance(barcode,previous_bc) for previous_bc in barcodes)<=args.distance:
+                barcode = generate_barcode(args.length, "")
         barcodes.append(barcode)
-    print(barcodes)
-    write_2_file(barcodes, args.output)
+        if args.verbosity >= 1:
+            print('Barcode {}: {}'.format(i+1, barcode))
+    write_2_file(barcodes, args.output, args.verbosity)
     return
 
 def get_arguments():
@@ -34,7 +35,16 @@ def get_arguments():
                         default="barcodes.txt")
     parser.add_argument("-d", "--distance", help="minimum Levenshtein distance between barcodes",
                         default=2, type=int)
+    parser.add_argument("-v", "--verbosity", help="adjust level of console output",
+                        default=0, type=int)
     args = parser.parse_args()
+    if args.verbosity >= 1:
+        print("\n--- Input arguments parsed ---")
+        print("Length: {}".format(args.length))
+        print("Number of barcodes: {}".format(args.numOfBc))
+        print("Minimum distance: {}".format(args.distance))
+        print("Output file: {}".format(args.output))
+        print("Verbosity: {}".format(args.verbosity))
     return args
 
 def generate_barcode(bc_len, bc):
@@ -47,10 +57,13 @@ def generate_barcode(bc_len, bc):
     bc += next_base
     return generate_barcode(bc_len-1, bc)
 
-def write_2_file(barcodes, file):
+def write_2_file(barcodes, file, verbosity):
     filehandle = open(file, 'w')
     formated_barcodes = ['bc_{}\t{}\n'.format(idx+1, barcode) for idx, barcode in enumerate(barcodes)]
     filehandle.writelines(formated_barcodes)
     filehandle.close()
+    if verbosity >= 1:
+        print("\n--- Saved results ---")
+        print("Saved barcodes to: {}".format(file))
 
 main()
